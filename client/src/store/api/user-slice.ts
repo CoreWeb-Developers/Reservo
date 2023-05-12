e604bc947ec440d3396cdedac775cc26f125de4e
Joseph-Gicuguma <josephgicuguma@gmail.com>
2023-05-12 21:55:29 +0300

    few more fixes

--- client/src/store/api/user-slice.ts
+++ client/src/store/api/user-slice.ts
@@ -1,4 +1,4 @@
-import { User, UsersParam, UsersResponse } from '~/types/user';
+import { User, UsersParam, UsersResponse, Service } from '~/types/user';
 import { apiSlice } from './api-slice';
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
@@ -16,19 +16,24 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         return { users, totalCount: Number(meta.response.headers.get('X-Total-Count')) };
       },
       providesTags: (_result, _err, arg) => {
-        const { eventId, companyId } = arg;
+        const { eventId, companyId, serviceId } = arg;
         const type =
           (eventId && ('EventSubscribers' as const)) ||
           (companyId && ('CompanySubscribers' as const)) ||
+          (serviceId && ('ServiceSubscribers' as const)) ||
           ('User' as const);
         const tag = {
-          id: eventId || companyId,
+          id: eventId || companyId || serviceId,
           type,
         };
         return [type, tag];
       },
     }),
+    getService: builder.query<Service, number>({
+      query: (id) => `/services/${id}`,
+      providesTags: (_result, _error, arg) => [{ type: 'Service' as const, id: arg }],
+    }),
   }),
 });
 
-export const { useGetUserQuery, useGetUsersQuery } = extendedApiSlice;
+export const { useGetUserQuery, useGetUsersQuery, useGetServiceQuery } = extendedApiSlice;
