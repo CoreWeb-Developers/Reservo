5314eaf66661e69b4a2c096e9d75297daf41e4e5
Joseph-Gicuguma <josephgicuguma@gmail.com>
2023-05-13 19:17:09 +0300

    few more

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,19 @@
-import Events from './Events/Events';
+import React from 'react';
+import './Home.css';
 import HomePromo from './HomePromo';
+import Events from './Events/Events';
+import Services from './Services/Services';
 
 const Home = () => {
   return (
     <>
       <HomePromo />
+      <div className="buttons-container">
+        <button className="button">Events</button>
+        <button className="button">Services</button>
+      </div>
       <Events />
+      <Services />
     </>
   );
 };
