d19cf6b0cddbc92771cd9a2542847c869fd6d7a9
Joseph-Gicuguma <josephgicuguma@gmail.com>
2023-05-19 21:33:10 +0300

    fewm more fixes

--- client/src/validation/service.ts
+++ client/src/validation/service.ts
@@ -11,14 +11,13 @@ const createSchema = z
     name: z.string().min(SERVICE_NAME_LENGTH.min).max(SERVICE_NAME_LENGTH.max),
     description: z.string().min(SERVICE_DESCRIPTION_LENGTH.min).max(SERVICE_DESCRIPTION_LENGTH.max),
     price: z.number({ invalid_type_error: 'Input price' }).min(0),
-    ticketsAvailable: z.number({ invalid_type_error: 'Input number of tickets' }).positive(),
+    slotsAvailable: z.number({ invalid_type_error: 'Input number of tickets' }).positive(),
     isNotificationsOn: z.boolean(),
     isPublic: z.boolean(),
     date: z.coerce.date().min(new Date(), { message: 'Set date in the future' }),
     publishDate: z.coerce.date(),
     latitude: z.number().min(LATITUDE.min).max(LATITUDE.max),
     longitude: z.number().min(LONGITUDE.min).max(LONGITUDE.max),
-    themeId: z.number().positive(),
   })
   .refine((data) => data.date >= data.publishDate, {
     message: 'Publication date must be before the service date',
